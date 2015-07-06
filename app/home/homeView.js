f821e9fa7124e56b8f4a5241fd7c7f5f9af91170
Patrick Kelly <patrick.kelly@vecna.com>
2015-07-06 10:00:47 -0400

    Will show up noew

--- app/home/homeView.js
+++ app/home/homeView.js
@@ -1,39 +1,28 @@
 var Torso = require('torso'),
     HomeModel = require('./homeModel'),
-    HomeView,
     Backbone = require('torso/node_modules/backbone');
 
-HomeView = Torso.ListView.extend({
-  template: require('./home-template.hbs'),
-
-  initialize: function() {
-   var MyCustomCollection = Torso.Collection.extend({
-    model: HomeModel,
-    url: "https://restful/fortune"
-   });
-   var self = this;
-   this.collection = new MyCustomCollection();
-   var myPrivateCollection = this.collection.createPrivateCollection();
-   console.log('Private Collection', myPrivateCollection);
-
-
-   myPrivateCollection.on('load-complete', function(myPrivateCollection){
-    console.log('RequesterCollection: ', this);
-   })
-   myPrivateCollection.fetch()
-   this.collection.on('sync', function(collection) {
-    console.log('Method 3 (Events)', collection.toJSON());
-    self.render();
-   });
-   this.collection.fetch();
-  },
-
-  prepare: function(){
-    console.log(this.collection.toJSON());
-    return this.collection.toJSON();
-  },
-
-
+HomeCollection = Torso.Collection.extend({
+  model: HomeModel,
+  url: "https://restful/fortune"
 });
 
-module.exports = new HomeView();
+var collection = new HomeCollection();
+var rCollection;
+this.listenTo(collection, 'loading-done', function(){
+  console.log('Listen Worked');
+});
+collection.fetch();
+
+
+HomeElementView = Torso.View.extend({
+  template: require('./_homeModelView.hbs')
+});
+
+HomeView = new Torso.ListView({
+  collection: rCollection,
+  childModel: 'model',
+  childView: HomeElementView,
+});
+
+module.exports = HomeView;
--- app/mockdata.js
+++ app/mockdata.js
@@ -20,6 +20,51 @@ $.mockjax({
       "name": 'Piper',
       "title": 'Miracle Worker',
       "workplace": 'Vecna Medical'
+    },
+    {
+      "name": 'Barry',
+      "title": 'Miracle Worker',
+      "workplace": 'Vecna Medical'
+    },
+    {
+      "name": 'Larry',
+      "title": 'Miracle Worker',
+      "workplace": 'Vecna Medical'
+    },
+    {
+      "name": 'Gary',
+      "title": 'Miracle Worker',
+      "workplace": 'Vecna Medical'
+    },
+    {
+      "name": 'Berry',
+      "title": 'Miracle Worker',
+      "workplace": 'Vecna Medical'
+    },
+    {
+      "name": 'Merry',
+      "title": 'Miracle Worker',
+      "workplace": 'Vecna Medical'
+    },
+    {
+      "name": 'Kerry',
+      "title": 'Miracle Worker',
+      "workplace": 'Vecna Medical'
+    },
+    {
+      "name": 'Perry',
+      "title": 'Miracle Worker',
+      "workplace": 'Vecna Medical'
+    },
+    {
+      "name": 'Terry',
+      "title": 'Miracle Worker',
+      "workplace": 'Vecna Medical'
+    },
+    {
+      "name": 'Zerry',
+      "title": 'Miracle Worker',
+      "workplace": 'Vecna Medical'
     }
   ]
 });
